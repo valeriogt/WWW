70df51c96e581d173774cedffed1746562fe9a55
valeriogt <vgtvaleriogarcia@gmail.com>
2024-11-13 12:38:08 +0100

    PRIMER COMI

---
+++ LENGUAJE DE MARCAS/dom.js
@@ -0,0 +1,12 @@
+let body = document.getElementsByTagName("body");
+let mainHeader = document.getElementById("mainHeader");
+let thunder = document.getElementsByClassName("thunder");
+
+console.log("Body" , body);
+console.log("mainHeader: ", mainHeader);
+console.log("thunder:", thunder);
+
+tagBody[0].style = "background-color00: lightcyan";
+
+document.getElementsByTagName("body")[0].style = "background-color: lightcyan,";
+tagMain[0].style.paddingTop = "1rem";
---
+++ LENGUAJE DE MARCAS/index.js
@@ -0,0 +1,50 @@
+alert("Hola");
+
+let product="ps5 pro";
+
+let price = "", dimensions = 0;
+
+product = "ps5";
+
+alert(product);
+
+let pokemon ="Pikachu";
+function showPokemon (nombrePokemon) {
+    //nombrePokemon = pokemon;
+    let pokemon = "Bulbasur";
+
+    console.log(nombrePokemon);
+}
+showPokemon(pokemon);
+
+let firstName = "Jon";
+let lastName = "Snow";
+let age = "24";
+
+console.log('Soy ' + firstName + ' ' + lastName + ' y tengo ' + age + ' años. ');
+
+function game0fThromePresentation (nombre, apellido, edad) {
+    console.log('Soy ${nombre} ${apellido} y tengo ${edad} años. ');
+
+    age+=6;
+    console.log('Soy', firstName, lastName, 'y tengo', age,  'años. ');
+
+    age-=6;
+    console.log('Soy', firstName, lastName, 'y tengo', age,  'años. ');
+
+    age*=10;
+    console.log('Soy', firstName, lastName, 'y tengo', age,  'años. ');
+
+    age/=2;
+    console.log('Soy', firstName, lastName, 'y tengo', age,  'años. ');
+
+    age++;
+    console.log('Soy', firstName, lastName, 'y tengo', age,  'años. ');
+
+    age+=25;
+    console.log('Soy', firstName, lastName, 'y tengo', age,  'años. ');
+
+    age/=5;
+    console.log('Soy', firstName, lastName, 'y tengo', age,  'años. ');
+
+}
